0c1e80a8f558c22b8e955a7d7b35dcd82b5f6f63
MatheusDeZottis <matheusdezottis@gmail.com>
2024-04-20 09:17:11 -0300

    exercícios da aula 01

---
+++ programação web back end/aula01/exerc01.js
@@ -0,0 +1,10 @@
+const n1 = 10
+const n2 = -30
+
+if (n1 <= 0) {
+    console.log("O número é zero")
+} else if (n2 >= 0) {
+    console.log("O número é positivo")
+} else {
+    console.log("O número é negativo")
+}
---
+++ programação web back end/aula01/exerc02.js
@@ -0,0 +1,9 @@
+const a = 30
+
+
+if (a >= 25) {
+    console.log("esta frio ")
+} else if (a <= 10) {
+    console.log("esta quente ")
+
+}
