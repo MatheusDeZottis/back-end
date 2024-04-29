c7a46413a4d270b2a037962455c8c2620104a1ab
MatheusDeZottis <matheusdezottis@gmail.com>
2024-04-29 20:40:19 -0300

    exemplos de função em javaScript

--- programação web back end/aula02/exerci03.js
+++ programação web back end/aula02/exerci03.js
@@ -1,10 +1,9 @@
 /* escreva um programa que calcule a soma dos números de 1 a 100 utilizando um loop */
-
 let soma = 0
-let numero = 1
+let numeroAtual = 1
 
-while (numero <= 100) {
-    soma += numero 
-    numero++
+while (numeroAtual <= 100) {
+    soma += numeroAtual 
+    numeroAtual++
 }
 console.log("A soma dos números de 1 a 100 é: " + soma)
