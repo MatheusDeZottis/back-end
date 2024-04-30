7a65641d0b12cccc2891e9ca6afc68daedb15e7f
MatheusDeZottis <matheusdezottis@gmail.com>
2024-04-29 21:30:51 -0300

    usando o arguments

--- programação web back end/aula02/function.js
+++ programação web back end/aula02/function.js
@@ -1,4 +1,4 @@
-function somar(){
+/*function somar(){
     let soma = 0 
     for (i in arguments) {
         soma += arguments[i]
@@ -10,3 +10,12 @@ console.log(somar(1,2))
 console.log(somar(1.1,2.2,3.3))
 console.log(somar('ola','mundo'))
 console.log(somar(3, 4, 'ja deu'))
+*/
+
+function soma (a , b) {
+    console.log(`O valor de a é ${a}`) // é executado
+    return a + b;
+    console.log(`o valor de b é ${b}`) // não executavel
+}
+let resultado = soma(3 ,5);
+console.log(resultado); //saída: 8 
