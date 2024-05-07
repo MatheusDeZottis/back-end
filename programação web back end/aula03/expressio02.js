20049f6d6388b508e58b6be3f73521890914f57b
MatheusDeZottis <matheusdezottis@gmail.com>
2024-05-07 07:40:47 -0300

    novos desafios mais informações nos cometarios iniciao de cada desafio

--- programação web back end/aula03/expressio02.js
+++ programação web back end/aula03/expressio02.js
@@ -1,16 +1,18 @@
 /* Crie uma expressão de função que calcule corretamnete a idade de uma  pessoa dada a data de aniversario sendo a string "dd/mm/yyy" (d=dia com 2digitos , M=me com 2 digitos, y=ano com 4 digitos ) */
 
-let dia = new Date()
-let mes = new Date() 
-let  ano = new Date()
-
-
-const idade = function (data){  // dd/mm/yyyyy
+const idade = function(data) {
+    const hoje = new Date()
+    const [dia, mes, ano] = data.split("/")
     
-    if(idade  += dia  += mes  === ano )
-    return dia + mes + ano
+    let idade = hoje.getFullYear() - parseInt(ano)
+    if (hoje.getMonth() < parseInt(mes) - 1) {
+        return idade - 1
+    } else if (hoje.getMonth() === (parseInt(mes) - 1)) {
+        if (hoje.getDate() < parseInt(dia)) {
+            return idade - 1
+        }
+    }
+    return idade
 }
-console.log(data)
-console.log(ano)
-console.log(`nascimento ${nancimento}`)
 
+console.log(idade('24/11/1999'))
